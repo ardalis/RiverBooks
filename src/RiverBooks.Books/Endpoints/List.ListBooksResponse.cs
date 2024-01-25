6617fb52841d5cb9bf1b4ded8dd37fbf14dace51
Steve Smith <steve@kentsmiths.com>
2024-01-25 10:41:34 -0500

    Clean  up and remove weather references

---
+++ src/RiverBooks.Books/Endpoints/List.ListBooksResponse.cs
@@ -0,0 +1,6 @@
+﻿namespace RiverBooks.Books.Endpoints;
+
+public class ListBooksResponse
+{
+    public IEnumerable<BookDto> Books { get; set; } = [];
+}
---
+++ src/RiverBooks.Books/Endpoints/List.cs
@@ -0,0 +1,21 @@
+﻿using FastEndpoints;
+
+namespace RiverBooks.Books.Endpoints;
+public class List(IBookService bookService) : 
+    EndpointWithoutRequest<ListBooksResponse>
+{
+    private readonly IBookService _bookService = bookService;
+
+    public override void Configure()
+    {
+        Get("/books");
+        
+        AllowAnonymous();
+    }
+
+    public override async Task HandleAsync(CancellationToken c)
+    {
+        var books = _bookService.GetBooks();
+        await SendAsync(new ListBooksResponse { Books = books });
+    }
+}

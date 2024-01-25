555709d010c2807dd34d7cdd58689feff5e28638
Steve Smith <steve@kentsmiths.com>
2024-01-25 10:20:08 -0500

    rename

--- src/RiverBooks.Books/ListBooks.cs
+++ src/RiverBooks.Books/ListBooks.cs
@@ -1,24 +1,12 @@
 ﻿using FastEndpoints;
-using Microsoft.AspNetCore.Builder;
 
 namespace RiverBooks.Books;
-
-public static class BookEndpoints
-{
-    public static void MapBookEndpoints(this WebApplication app)
-    {
-        app.MapGet("/books", (IBookService bookService) =>
-        {
-            return bookService.GetBooks();
-        });
-    }
-}
 
 public class MyBookResponse
 {
     public IEnumerable<BookDto> Books { get; set; } = [];
 }
-public class MyBookEndpoint(IBookService bookService) : EndpointWithoutRequest<MyBookResponse>
+public class ListBooks(IBookService bookService) : EndpointWithoutRequest<MyBookResponse>
 {
     private readonly IBookService _bookService = bookService;
 

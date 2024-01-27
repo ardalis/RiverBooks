804f2471b43d9f3fb6a29b0684cf32a412ac731d
Steve Smith <steve@kentsmiths.com>
2024-01-27 18:37:18 -0500

    Added Book entity

---
+++ src/RiverBooks.Books/Book.cs
@@ -0,0 +1,34 @@
+﻿using Ardalis.GuardClauses;
+
+namespace RiverBooks.Books;
+
+internal class Book
+{
+  public Guid Id { get; private set; } = Guid.NewGuid();
+  public string Title { get; private set; } = string.Empty;
+  public string Author { get; private set; } = string.Empty;
+  public decimal Price { get; private set; }
+
+  internal Book(Guid id, string title, string author, decimal price)
+  {
+    Id = Guard.Against.Default(id);
+    Title = Guard.Against.NullOrEmpty(title);
+    Author = Guard.Against.NullOrEmpty(author);
+    Price = Guard.Against.Negative(price);
+  }
+
+  internal void UpdateTitle(string newTitle)
+  {
+    Title = Guard.Against.NullOrEmpty(newTitle);
+  }
+
+  internal void UpdateAuthor(string newAuthor)
+  {
+    Author = Guard.Against.NullOrEmpty(newAuthor);
+  }
+
+  internal void UpdatePrice(decimal newPrice)
+  {
+    Price = Guard.Against.Negative(newPrice);
+  }
+}

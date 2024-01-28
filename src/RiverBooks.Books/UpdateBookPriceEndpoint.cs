a637aabd6ec516ebc38a7731d214c080b258e2aa
Steve Smith <steve@kentsmiths.com>
2024-01-28 14:04:11 -0500

    Implemented price updates as a POST to pricehistory

---
+++ src/RiverBooks.Books/UpdateBookPriceEndpoint.cs
@@ -0,0 +1,25 @@
+﻿using FastEndpoints;
+
+namespace RiverBooks.Books;
+
+internal class UpdateBookPriceEndpoint(IBookService bookService) :
+    Endpoint<UpdateBookPriceRequest, BookDto>
+{
+  private readonly IBookService _bookService = bookService;
+
+  public override void Configure()
+  {
+    Post("/books/pricehistory"); // we're not actually tracking price history but we could in the future
+    AllowAnonymous();
+  }
+
+  public override async Task HandleAsync(UpdateBookPriceRequest request,
+             CancellationToken cancellationToken = default)
+  {
+    await _bookService.UpdateBookPriceAsync(request.Id, request.NewPrice);
+    
+    var updatedBook = await _bookService.GetBookByIdAsync(request.Id);
+
+    await SendAsync(updatedBook);
+  }
+}
---
+++ src/RiverBooks.Books/UpdateBookPriceRequest.cs
@@ -0,0 +1,3 @@
+﻿namespace RiverBooks.Books;
+
+public record UpdateBookPriceRequest(Guid Id, decimal NewPrice);

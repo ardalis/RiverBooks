2f61116cb20ad436a63ca60bfd3bfec2244a7012
Steve Smith <steve@kentsmiths.com>
2024-01-29 20:53:28 -0500

    Added Serilog

--- src/RiverBooks.Web/Program.cs
+++ src/RiverBooks.Web/Program.cs
@@ -1,8 +1,18 @@
 ﻿using FastEndpoints;
 using FastEndpoints.Swagger;
 using RiverBooks.Books;
+using Serilog;
+
+var logger = Log.Logger = new LoggerConfiguration()
+  .Enrich.FromLogContext()
+  .WriteTo.Console()
+  .CreateLogger();
+
+logger.Information("Starting web host");
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog((_, config) => config.ReadFrom.Configuration(builder.Configuration));
 
 builder.Services.AddFastEndpoints()
     .SwaggerDocument();

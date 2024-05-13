6c7fca80b3501d541e195289cb2123ccbfb88bdb
Steve Smith <steve@kentsmiths.com>
2024-05-13 13:24:22 -0400

    Add some simple tests

---
+++ src/RiverBooks.Books.Tests/Entities/BookUpdateTitle.cs
@@ -0,0 +1,20 @@
+﻿namespace RiverBooks.Books.Tests.Entities;
+
+public class BookUpdateTitle
+{
+  [Fact]
+  public void ThrowsGivenEmptyTitle()
+  {
+    var book = new Book(Guid.NewGuid(), "title", "author", 1m);
+    Assert.Throws<ArgumentException>(() => book.UpdateTitle(""));
+  }
+
+  [Fact]
+  public void ThrowsGivenNullTitle()
+  {
+    var book = new Book(Guid.NewGuid(), "title", "author", 1m);
+#pragma warning disable CS8625 // Cannot convert null literal to non-nullable reference type.
+    Assert.Throws<ArgumentNullException>(() => book.UpdateTitle(null));
+#pragma warning restore CS8625 // Cannot convert null literal to non-nullable reference type.
+  }
+}
